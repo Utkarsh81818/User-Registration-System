a1493da080ef5bedc908e2e948a820f28974d793
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-30 01:40:26 +0530

    Added: Should have atleast 1 Special Character in the password

--- src/com/bridgelabz/regex/UserRegistrationMain.java
+++ src/com/bridgelabz/regex/UserRegistrationMain.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.regex;
 import java.util.Scanner;
 /**
- * Purpose  - Should have at least 1 numeric number in the password
+ * Purpose  - Should have atleast 1 Special Character in the password
  * @author  - Utkarsh Mishra
  * @version - 16.0
  * @since   - 2021-09-29
@@ -23,6 +23,7 @@ public class UserRegistrationMain {
         System.out.println("Enter 5 : to validate Password Rule 1 ");
         System.out.println("Enter 6 : to validate Password Rule 2 ");
         System.out.println("Enter 7 : to validate Password Rule 3 ");
+        System.out.println("Enter 8 : to validate Password Rule 4 ");
         switch (scanner.nextInt()){
             case 1:
                 UserRegistration.validFirstName();
@@ -45,8 +46,11 @@ public class UserRegistrationMain {
             case 7:
                 UserRegistration.validPassRule3();
                 break;
+            case 8:
+                UserRegistration.validPassRule4();
+                break;
             default:
-                System.out.println("Select a valid number");
+                System.out.println("Invalid! Select a valid number");
         }
     }
 }
