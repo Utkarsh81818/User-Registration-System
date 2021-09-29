cfd5ae8ddcb51b8852d0001b7632ec3da7847079
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-30 00:15:29 +0530

    Added: Enter a valid last name

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -9,7 +9,9 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static final Scanner scanner = new Scanner(System.in);
-
+    /*
+    Created a method to validate first name
+     */
     public static void validFirstName(){
         System.out.print("Enter the First Name : ");
         String name = scanner.next();
@@ -21,5 +23,22 @@ public class UserRegistration {
             System.out.println("First name is valid");
         else
             System.out.println("First name is !Invalid");
+    }
+    /*
+    Created a method to validate last name
+     */
+    public static void validLastName(){
+        System.out.println("Enter the Last Name");
+        String lname = scanner.next();
+
+        String regex1 = "^[A-Z]{1}[a-z]{2,}$";
+
+        Pattern p1 = Pattern.compile(regex1);
+        Matcher m1 = p1.matcher(lname);
+        boolean r1 = m1.matches();
+        if (r1)
+            System.out.println("Last name is Valid");
+        else
+            System.out.println("Last name is Invalid");
     }
 }
--- src/com/bridgelabz/regex/UserRegistrationMain.java
+++ src/com/bridgelabz/regex/UserRegistrationMain.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.regex;
 import java.util.Scanner;
 /**
- * Purpose  - As a User need to enter a valid First Name
+ * Purpose  - As a User need to enter a valid Last Name
  * @author  - Utkarsh Mishra
  * @version - 16.0
  * @since   - 2021-09-29
@@ -17,10 +17,16 @@ public class UserRegistrationMain {
         UserRegistration user = new UserRegistration();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 : to validate First Name ");
+        System.out.println("Enter 2 : to validate Last Name ");
         switch (scanner.nextInt()){
             case 1:
                 UserRegistration.validFirstName();
                 break;
+            case 2:
+                UserRegistration.validLastName();
+                break;
+            default:
+                System.out.println("Select a valid number");
         }
     }
 }
