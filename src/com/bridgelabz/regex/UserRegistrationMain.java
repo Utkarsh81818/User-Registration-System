3b668cf7dbc75ef0d1425a2f65f7b8a8aeb34a1c
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-30 00:25:17 +0530

    Added: User need to enter a valid E-mail ID

--- src/com/bridgelabz/regex/UserRegistrationMain.java
+++ src/com/bridgelabz/regex/UserRegistrationMain.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.regex;
 import java.util.Scanner;
 /**
- * Purpose  - As a User need to enter a valid Last Name
+ * Purpose  - As a User need to enter a valid E-mail ID
  * @author  - Utkarsh Mishra
  * @version - 16.0
  * @since   - 2021-09-29
@@ -18,12 +18,16 @@ public class UserRegistrationMain {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 : to validate First Name ");
         System.out.println("Enter 2 : to validate Last Name ");
+        System.out.println("Enter 3 : to validate E-mail Address ");
         switch (scanner.nextInt()){
             case 1:
                 UserRegistration.validFirstName();
                 break;
             case 2:
                 UserRegistration.validLastName();
+                break;
+            case 3:
+                UserRegistration.validEmailId();
                 break;
             default:
                 System.out.println("Select a valid number");
