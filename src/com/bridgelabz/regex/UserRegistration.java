b71a0afb7aecac8fb60fe5f7fd485cee5de1682c
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-30 00:35:55 +0530

    Added: User need to enter a mobile number in a pre-defined format

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 /**
- * Purpose  - As a User need to enter a valid Email Address
+ * Purpose  - As a User need to enter a mobile number in a pre-defined format.
  */
 
 public class UserRegistration {
@@ -56,5 +56,21 @@ public class UserRegistration {
             System.out.println("Email Address is Valid");
         else
             System.out.println("Email Address is Invalid");
+    }
+    /*
+     Created a method to validate Mobile Number
+     */
+    public static void validMobileNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your mobile number : ");
+        String phoneNo = scanner.nextLine();
+        String regex3 = "^[0-9]{2}\\s{0,1}[0-9]{10}$";
+        Pattern p3 = Pattern.compile(regex3);
+        Matcher m3 = p3.matcher(phoneNo);
+        boolean r3 = m3.matches();
+        if (r3)
+            System.out.println("Phone number is Valid");
+        else
+            System.out.println("Phone number is Invalid");
     }
 }
--- src/com/bridgelabz/regex/UserRegistrationMain.java
+++ src/com/bridgelabz/regex/UserRegistrationMain.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.regex;
 import java.util.Scanner;
 /**
- * Purpose  - As a User need to enter a valid E-mail ID
+ * Purpose  - As a User need to enter a mobile number in a pre-defined format.
  * @author  - Utkarsh Mishra
  * @version - 16.0
  * @since   - 2021-09-29
@@ -19,6 +19,7 @@ public class UserRegistrationMain {
         System.out.println("Enter 1 : to validate First Name ");
         System.out.println("Enter 2 : to validate Last Name ");
         System.out.println("Enter 3 : to validate E-mail Address ");
+        System.out.println("Enter 4 : to validate Mobile Number ");
         switch (scanner.nextInt()){
             case 1:
                 UserRegistration.validFirstName();
@@ -28,6 +29,9 @@ public class UserRegistrationMain {
                 break;
             case 3:
                 UserRegistration.validEmailId();
+                break;
+            case 4:
+                UserRegistration.validMobileNumber();
                 break;
             default:
                 System.out.println("Select a valid number");
