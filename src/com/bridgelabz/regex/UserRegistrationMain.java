59d28503249b392ad935d1c525c28365c51b013f
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-30 00:03:26 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/regex/UserRegistrationMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.regex;
+
+public class UserRegistrationMain {
+
+    public static void welcome(){
+        System.out.println("Welcome to User Registration System Problem");
+    }
+    public static void main(String[] args) {
+	welcome();
+    }
+}
